3fbf63872f402398f7422f919193bbb82734f7f8
Tyuiu.IbrayevAA@outlook.com <03.05.05.ibraev.abai@gmail.com>
2023-09-14 19:30:32 +0500

    Task7

--- Tyuiu.IbrayevAA.Sprint0.Task7.V0.Test/Tyuiu.IbrayevAA.Sprint0.Task7.V0.Test.cpp
+++ Tyuiu.IbrayevAA.Sprint0.Task7.V0.Test/Tyuiu.IbrayevAA.Sprint0.Task7.V0.Test.cpp
@@ -1,16 +1,27 @@
 #include "pch.h"
 #include "CppUnitTest.h"
+#include "../Tyuiu.IbrayevAA.Sprint0.Task7.V0.Lib/Tyuiu.IbrayevAA.Sprint0.Task7.V0.Lib.cpp"
+
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-namespace TyuiuIbrayevAASprint0Task7V0Test
+namespace UnitTest
 {
-	TEST_CLASS(TyuiuIbrayevAASprint0Task7V0Test)
+	TEST_CLASS(UnitTest)
 	{
 	public:
-		
+
 		TEST_METHOD(TestMethod1)
 		{
+			// Init
+			ISprint0Task7* add = new Service();
+			int a{};
+			int b{};
+			int v{};
+
+			//run
+			v = add->Rezalt(a, b);
+			Assert::AreEqual(v, v);
 		}
 	};
 }
